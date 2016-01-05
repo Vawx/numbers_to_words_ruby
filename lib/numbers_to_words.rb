e58ad9410eb719a0b13a4b47abc545c41e504aca
Kyle Langley and Riley Starnes <altck8@gmail.com>
2016-01-05 14:10:36 -0800

    converts numbers between 100 and 999 to text

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -18,6 +18,7 @@ class Fixnum
                   17 => "seventeen",
                   18 => "eighteen",
                   19 => "nineteen"}
+
   by_tens =     { 20 => "twenty",
                   30 => "thirty",
                   40 => "fourty",
@@ -25,15 +26,17 @@ class Fixnum
                   60 => "sixty",
                   70 => "seventy",
                   80 => "eighty",
-                  90 => "ninety"}
+                  90 => "ninety" }
 
   define_method(:numbers_to_words) do
-  if ( self <= 19 && self >= 0 )
-    return to_nineteen.fetch( self )
-  elsif( self > 19 && self < 100 )
-    return get_by_tens( self )
+    if ( self <= 19 && self >= 0 )
+      return to_nineteen.fetch( self )
+    elsif( self > 19 && self < 100 )
+      return get_by_tens( self )
+    elsif( self > 100 && self < 1000 )
+      return get_by_hundreds( self )
+    end
   end
-end
 
   define_method(:get_by_tens) do |tens|
     remaining = tens % 10
@@ -42,6 +45,17 @@ end
     else
       tens -= remaining
       return by_tens.fetch(tens) + " " + to_nineteen.fetch(remaining)
+    end
+  end
+
+  define_method(:get_by_hundreds) do |hundreds|
+    remaining = hundreds % 100
+    hundreds -= remaining
+    hundreds /= 100
+    if remaining > 19
+      return to_nineteen.fetch( hundreds ) + " hundred and " + get_by_tens( remaining )
+    else
+      return to_nineteen.fetch( hundreds ) + " hundred and " + to_nineteen.fetch( remaining )
     end
   end
 
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,6 +1,18 @@
 require('rspec')
 require('numbers_to_words')
 
+describe("String#by_tens") do
+  it ('converts a numerical value between 20 and 99') do
+    expect(96.numbers_to_words).to(eq('ninety six'))
+  end
+end
+
+describe("String#by_hundreds") do
+  it('converts a numerical value between 100 and 999') do
+    expect(534.numbers_to_words).to(eq("five hundred and thirty four"))
+  end
+end
+
 describe("String#numbers_to_words") do
   it ('converts a numerical value into text read as the value') do
     expect(1.numbers_to_words).to(eq('one'))
@@ -10,5 +22,8 @@ describe("String#numbers_to_words") do
   end
   it ('converts a numerical value above twenty into text as the value') do
     expect(99.numbers_to_words).to(eq('ninety nine'))
+  end
+  it ('converts a numerical value above twenty into text as the value') do
+    expect(739.numbers_to_words).to(eq('seven hundred and thirty nine'))
   end
 end
