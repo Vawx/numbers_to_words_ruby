6bb5cee046f3075ae314ee50dcd1618ce4a24c2b
Kyle Langley and Riley Starnes <altck8@gmail.com>
2016-01-05 13:22:47 -0800

    convert a numerical value into text

---
+++ spec/numbers_to_words_spec.rb
@@ -0,0 +1,11 @@
+require('rspec')
+require('numbers_to_words')
+
+describe("String#numbers_to_words") do
+  it ('converts a numerical value into text read as the value') do
+    expect(1.numbers_to_words).to(eq('one'))
+  end
+  it ('converts a numerical value above twenty into text as the value') do
+    expect(20.numbers_to_words).to(eq('twenty'))
+  end
+end
